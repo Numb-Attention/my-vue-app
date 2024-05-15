eda47ce997046403b913a67641e2ec9039cf7e98
Numb <1627046612@qq.com>
2024-05-15 17:13:37 +0800

    fix:修改

--- src/pages/role/list.vue
+++ src/pages/role/list.vue
@@ -8,107 +8,52 @@
 
 			<el-table-column label="状态" width="120">
 				<template #default="{ row }">
-					<el-switch
-						:modelValue="row.status"
-						:active-value="1"
-						:inactive-value="0"
-						@change="handleStatusChange($event, row)"
-						:loading="row.statusLoading"
-						:disabled="row.super == 1"
-					>
+					<el-switch :modelValue="row.status" :active-value="1" :inactive-value="0"
+						@change="handleStatusChange($event, row)" :loading="row.statusLoading"
+						:disabled="row.super == 1">
 					</el-switch>
 				</template>
 			</el-table-column>
 
 			<el-table-column prop="address" label="操作" width="250" align="center">
 				<template #default="scope">
-					<el-button
-						type="primary"
-						size="small"
-						text
-						@click="openSetRule(scope.row)"
-						>配置权限</el-button
-					>
-					<el-button
-						type="primary"
-						size="small"
-						text
-						@click="handleEdit(scope.row)"
-						>修改</el-button
-					>
-					<el-popconfirm
-						title="是否要删除该公告"
-						confirm-button-text="确认"
-						cancel-button-text="取消"
-						@confirm="handleDelet(scope.row.id)"
-					>
+					<el-button type="primary" size="small" text @click="openSetRule(scope.row)">配置权限</el-button>
+					<el-button type="primary" size="small" text @click="handleEdit(scope.row)">修改</el-button>
+					<el-popconfirm title="是否要删除该公告" confirm-button-text="确认" cancel-button-text="取消"
+						@confirm="handleDelet(scope.row.id)">
 						<template #reference>
-							<el-button class="px-1" text type="primary" size="small"
-								>删除</el-button
-							>
+							<el-button class="px-1" text type="primary" size="small">删除</el-button>
 						</template>
 					</el-popconfirm>
 				</template>
 			</el-table-column>
 		</el-table>
 		<div class="flex items-center justify-center mt-5">
-			<el-pagination
-				background
-				layout="prev,pager, next"
-				:total="total"
-				:current-page="currentPage"
-				:page-size="limit"
-				@current-change="getData"
-			/>
+			<el-pagination background layout="prev,pager, next" :total="total" :current-page="currentPage"
+				:page-size="limit" @current-change="getData" />
 		</div>
 
 		<FormDrawer ref="formDrawerRef" :title="drawerTitle" @submit="handleSubmit">
-			<el-form
-				:model="form"
-				ref="formRef"
-				:rules="rules"
-				label-width="80px"
-				:inline="false"
-			>
+			<el-form :model="form" ref="formRef" :rules="rules" label-width="80px" :inline="false">
 				<el-form-item label="角色名称" prop="name">
 					<el-input v-model="form.name" placeholder="角色名称"></el-input>
 				</el-form-item>
 				<el-form-item label="角色描述" prop="desc">
-					<el-input
-						v-model="form.desc"
-						placeholder="角色描述"
-						type="textarea"
-						:rows="5"
-					></el-input>
+					<el-input v-model="form.desc" placeholder="角色描述" type="textarea" :rows="5"></el-input>
 				</el-form-item>
 				<el-form-item label="状态" prop="status">
-					<el-switch
-						v-model="form.status"
-						:active-value="1"
-						:inactive-value="0"
-					>
+					<el-switch v-model="form.status" :active-value="1" :inactive-value="0">
 					</el-switch>
 				</el-form-item>
 			</el-form>
 		</FormDrawer>
 
 		<!-- 权限配置 -->
-		<FormDrawer
-			ref="setRuleFormDrawerRef"
-			title="权限配置"
-			@submit="handleSetRuleSubmit"
-		>
-			<el-tree-v2
-				:check-strictly="checkStrictly"
-				ref="elTreeRef"
-				node-key="id"
-				:default-expanded-keys="defaultExpandedKeys"
-				:data="ruleList"
-				:props="{ label: 'name', children: 'child' }"
-				show-checkbox
-				:height="treeHeight"
-				@check="handleTreeCheck"
-			>
+		<FormDrawer ref="setRuleFormDrawerRef" title="权限配置" @submit="handleSetRuleSubmit">
+			<el-tree-v2 :check-strictly="checkStrictly" ref="elTreeRef" node-key="id"
+				:default-expanded-keys="defaultExpandedKeys" :data="ruleList"
+				:props="{ label: 'name', children: 'child' }" show-checkbox :height="treeHeight"
+				@check="handleTreeCheck">
 				<template #default="{ node, data }">
 					<div class="flex items-center">
 						<el-tag :type="data.menu ? '' : 'info'" size="small">
